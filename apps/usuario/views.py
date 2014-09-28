2e5521cec78b6b8edcbe9c49112dc4421bdb3506
Lourdes <lumil@hotmail.com>
2014-09-28 18:43:48 -0500

    primer commit

---
+++ apps/usuario/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.shortcuts import RequestContext
+# Create your views here.
+def pagina_principal(request):
